6c50548f35f4c0574679b87f5cced820a255bdcd
Şeyda Arı <ariseydaari@gmail.com>
2020-06-16 23:16:50 +0300

    Calculator

---
+++ Calculator.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<math.h>
+
+//Calculator
+// First input is an integer. Second input is the operation character. 
+//Third input is the second integer. Valid operatos are +, -, *, /, and %. 
+
+int main(){
+	
+	int x,y;
+	char n;
+	float result;
+	
+	printf("Please enter an integer:");
+	scanf("%d",&x);
+	printf("Please enter a valid operator(+,-,*,/,%):");
+	scanf("%s",&n);
+	printf("Please enter an integer:");
+	scanf("%d",&y);
+	
+	if(n=='+' || n=='-' || n=='*' || n=='/' || n=='%'){
+		
+		switch(n){
+			case '+':
+			   result=x+y;
+			   printf("x+y:%.2f",result);	
+			   break;
+			case '-':
+			   result=x-y;
+			   printf("x-y:%.2f",result);
+			   break;
+			case '*':
+			   result=x*y;
+			   printf("x*y:%.2f",result);
+			   break;
+			case '/':
+			   result=x/y;
+			   printf("x/y:%.2f",result);
+			   break;
+			case '%':
+			   result=x%y;
+			   printf("x%y:%.2f",result);
+			   break;   
+			default :
+				break;
+			            
+		}
+	}
+	else
+	   printf("Please enter a valid character.");
+	
+	return 0;
+}
+
